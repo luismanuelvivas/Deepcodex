78a0a99433aa3c12eafb6722b59978575de8f70d
Luis Manuel Vivas <78003626+luismanuelvivas@users.noreply.github.com>
2025-04-14 20:07:24 -0400

    ...

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "844021ea4dc9bd4b"; }
+/******/ 		__webpack_require__.h = function() { return "5a8c1c8cb44e263c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
